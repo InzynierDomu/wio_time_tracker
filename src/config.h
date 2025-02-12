a636e7f37d4ea04522a2cc266f5d326b6d086fec
Inżynier Domu <inzynier.domu@gmail.com>
2025-02-12 14:43:18 +0100

    add schem

--- src/config.h
+++ src/config.h
@@ -5,6 +5,6 @@ namespace config
 const String counter_list_path = "/config.json";
 const String wifi_config_path = "/wifi.json";
 const unsigned int localPort = 2390; // local port to listen for UDP packets
-const char timeServer[] = "tempus1.gum.gov.pl"; // extenral NTP server e.g. time.nist.gov
+const char timeServer[] = "tempus1.gum.gov.pl"; // extenral NTP server
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 } // namespace config
